ec1d62105b48e6efd1bac3aeff86e2864d8549d5
Reese <arter.charise@gmail.com>
2021-01-08 01:28:54 -0600

    update contact complete

--- routes/contacts.js
+++ routes/contacts.js
@@ -54,8 +54,38 @@ router.post(
 // @route   POST  api/contacts/:id
 // @desc    Update specific contact
 // @access  Private
-router.put("/:id", (req, res) => {
-	res.send("Update specific contact");
+router.put("/:id", auth, async (req, res) => {
+	const { name, email, phone, type } = req.body;
+
+	//build contact object
+
+	const contactFields = {};
+	if (name) contactFields.name = name;
+	if (email) contactFields.email = email;
+	if (phone) contactFields.phone = phone;
+	if (type) contactFields.type = type;
+
+	try {
+		let contact = await Contact.findById(req.params.id);
+
+		if (!contact) {
+			return res.status(404).json({ msg: "Contact not found" });
+		}
+		//make sure user owns contact
+
+		if (contact.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "Not Authorized" });
+		}
+		contact = await Contact.findByIdAndUpdate(
+			req.params.id,
+			{ $set: contactFields },
+			{ new: true }
+		);
+		res.json(contact);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
 });
 
 // @route   DELETE  api/contacts/:id
