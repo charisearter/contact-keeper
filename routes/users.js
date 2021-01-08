642bfad49e1b618eaf7e86eb7662d82542c46e27
Reese <arter.charise@gmail.com>
2021-01-07 23:48:49 -0600

    added user hash password and register route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
@@ -17,12 +18,36 @@ router.post(
 			"Please enter a password with 6 or more characters"
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send("passed");
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+				res.status(400).json({ msg: "User already exists" });
+			}
+
+			user = new User({
+				name,
+				email,
+				password,
+			});
+			// encrypt password
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+			//save to DB
+			await user.save();
+			res.send("user saved");
+		} catch (err) {
+			console.error(err.message);
+			req.status(500).send("Server error");
+		}
 	}
 );
 
